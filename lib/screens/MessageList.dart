6d1d2ab2d91ee181d4a7f2d9a2cd7c0c8ceecf26
Oli <dev.olivercarlos@gmail.com>
2020-01-29 15:03:31 +0800

    Convert MessageList from Stateless Widget to Stateful Widget.

--- lib/screens/MessageList.dart
+++ lib/screens/MessageList.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
 
-class MessageList extends StatelessWidget {
+class MessageList extends StatefulWidget {
   final String title;
-  var messages = const []; // This should come from an API endpoint.
 
   MessageList({Key key, this.title}) : super(key: key);
 
   @override
+  _MessageListState createState() => _MessageListState();
+}
+
+class _MessageListState extends State<MessageList> {
+  var messages = const []; 
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text(title),
+          title: Text(widget.title),
         ),
         body: ListView.separated(
           itemCount: messages.length,
@@ -23,7 +28,11 @@ class MessageList extends StatelessWidget {
                 child: Text('O'),
               ),
               title: Text(message['title']),
-              subtitle: Text(message['body'], maxLines: 2, overflow: TextOverflow.ellipsis  ,),
+              subtitle: Text(
+                message['body'],
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+              ),
             );
           },
         ));
